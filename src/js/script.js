10c947fddae3355f78939563078e53186620755d
Adrian <klimasa98@gmail.com>
2023-11-03 19:55:51 -0400

    created aboutus section

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,23 @@
 const hamburgerIcon = document.querySelector('.navbar__btn');
 const navWindow = document.querySelector('.nav');
 const burgerBox = document.querySelector('.navbar__box');
+const windwo = document.querySelector('.windwo');
+const allNavLinks = document.querySelectorAll('.nav__item')
 
 hamburgerIcon.addEventListener('click', () => {
-	navWindow.classList.toggle('activeNavWindow');
-	burgerBox.classList.toggle('activeBurgerIcon');
+
+	
+	windwo.classList.toggle('activeWinwdo');
+	setTimeout(() => {
+		navWindow.classList.toggle('activeNavWindow');
+		burgerBox.classList.toggle('activeBurgerIcon');
+	}, 1);
 });
+
+allNavLinks.forEach(link => link.addEventListener('click', ()=>{
+	windwo.classList.toggle('activeWinwdo');
+	setTimeout(() => {
+		navWindow.classList.toggle('activeNavWindow');
+		burgerBox.classList.toggle('activeBurgerIcon');
+	}, 1);
+}))
