619c7552170eeb71b3ca6d8cb432f513f411b03e
Adrian <klimasa98@gmail.com>
2023-11-04 20:02:46 -0400

    created menu photo slider secton

---
+++ src/js/slick.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+	$('.reviews__boxes').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		arrows: false,
+		mobileFirst: true,
+		responsive: [
+			{
+				breakpoint: 700,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+				},
+			},
+			{
+				breakpoint: 550,
+				settings:{
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				},
+			}
+		],
+	})
+})
