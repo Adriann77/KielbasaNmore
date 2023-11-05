2fb4bbb3767449e24055a20d0c8605f199fcec94
Adrian <klimasa98@gmail.com>
2023-11-05 06:36:16 -0500

    fixed bug with wrapper and background-attachemtn

--- src/js/slick.js
+++ src/js/slick.js
@@ -3,21 +3,28 @@ $(document).ready(function () {
 		slidesToShow: 1,
 		slidesToScroll: 1,
 		autoplay: true,
-		autoplaySpeed: 2000,
-		arrows: false,
+		autoplaySpeed: 2500,
+		arrows: true,
 		mobileFirst: true,
 		responsive: [
 			{
 				breakpoint: 700,
 				settings: {
-					slidesToShow: 3,
+					slidesToShow: 2,
 					slidesToScroll: 1,
+				},
+			},
+			{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 2,
 				},
 			},
 			{
 				breakpoint: 550,
 				settings:{
-					slidesToShow: 2,
+					slidesToShow: 1,
 					slidesToScroll: 1,
 				},
 			}
